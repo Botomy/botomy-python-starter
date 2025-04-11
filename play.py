a90f2520449bf82d4397ad62714a211798849b8c
jason <cintari@gmail.com>
2025-04-11 09:00:56 -0700

    Update congrats message

--- play.py
+++ play.py
@@ -13,7 +13,7 @@ def play(level_data: LevelData) -> List[Move]:
 
     moves.append({
       "debug_info": {
-          "message": "Congratulations! You have reached the play function. Modify this (play.py) to implement your bot's logic. Try adding move_to: {x,y} to your move array",
+          "message": "Congratulations! You have reached the play function. Modify this file (play.py) to implement your bot's logic.",
       }
     })
     return moves
