d72b908f10070fc39042823d2064ba67aa2c5af4
jason <cintari@gmail.com>
2025-04-10 21:02:12 -0700

    Improve indentation

--- example_bot.py
+++ example_bot.py
@@ -21,41 +21,41 @@ def get_closest_item(position: Position, items: List[GameObject]) -> Optional[Ga
     return target
 
 def play(level_data: LevelData) -> List[Move]:
-  moves = []
-  own_player = level_data.own_player
-  players = level_data.players
-  items = level_data.items
+    moves = []
+    own_player = level_data.own_player
+    players = level_data.players
+    items = level_data.items
 
-  # Build a list of potential targets (items, enemies, etc.)
-  potential_targets = items + level_data.enemies
+    # Build a list of potential targets (items, enemies, etc.)
+    potential_targets = items + level_data.enemies
 
-  # Get the closest item to your own player's position
-  target = get_closest_item(own_player.position, potential_targets)
+    # Get the closest item to your own player's position
+    target = get_closest_item(own_player.position, potential_targets)
 
-  if target:
-      if target.health != None:
-          if dist_squared_to(own_player.position, target.position) < 15625:
-              moves.append("attack")
-              moves.append("shield")
-      moves.append({"move_to": target.position})
-  else:
-      print("no target found")
+    if target:
+        if target.health != None:
+            if dist_squared_to(own_player.position, target.position) < 15625:
+                moves.append("attack")
+                moves.append("shield")
+        moves.append({"move_to": target.position})
+    else:
+        print("no target found")
 
-  if own_player.health / own_player.max_health < 0.51:
-      # health is less than 50% so use the potion to heal
-      moves.append({"use": "big_potion"})
+    if own_player.health / own_player.max_health < 0.51:
+        # health is less than 50% so use the potion to heal
+        moves.append({"use": "big_potion"})
 
-  if own_player.levelling.available_skill_points > 0:
-      # skill points available - level up
-      skill = random.choice(["attack", "health", "speed"])
-      moves.append({"redeem_skill_point": skill})
+    if own_player.levelling.available_skill_points > 0:
+        # skill points available - level up
+        skill = random.choice(["attack", "health", "speed"])
+        moves.append({"redeem_skill_point": skill})
 
-  moves.append({"speak": "Hello Botomy!"})
+    moves.append({"speak": "Hello Botomy!"})
 
-  moves.append({
-      "debug_info": {
-          "target_id": target.id if target else None,
-          "message": "oh hai"
-      }
-  })
-  return moves
+    moves.append({
+        "debug_info": {
+            "target_id": target.id if target else None,
+            "message": "oh hai"
+        }
+    })
+    return moves
--- play.py
+++ play.py
@@ -3,12 +3,17 @@ from typing import List
 
 
 def play(level_data: LevelData) -> List[Move]:
-  moves = []
-  moves.append({"speak": "Hello Botomy!"})
+    moves = []
+    own_player = level_data.own_player
+    players = level_data.players
+    items = level_data.items
+    enemies = level_data.enemies
+    
+    moves.append({"speak": "Hello Botomy!"})
 
-  moves.append({
+    moves.append({
       "debug_info": {
           "message": "oh hai"
       }
-  })
-  return moves
+    })
+    return moves
